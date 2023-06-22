1f5ec861d8f8bab7e842f3d3cd10eca8743f2803
ItsIceCreeperPE <phenyosethole0907@gmail.com>
2023-06-22 14:36:22 +0200

    Added red theme from zdkrimson launcher

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from nicegui import ui
 
+# Theming
+ui.query('body').style('background-color: #630000')
+ui.colors(primary='#bf3a3a')
+
 ui.markdown('# ***zdkrimson : A Minecraft Launcher***')
 
 ui.run(dark=True, native=True)
