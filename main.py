c9c74c98ef0b1a9baf3a1c5b9e77b4ed1f91aaa6
ItsIceCreeperPE <phenyosethole0907@gmail.com>
2023-06-23 20:22:49 +0200

    Added logo

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ from nicegui import ui
 ui.query('body').style('background-color: #630000')
 ui.colors(primary='#bf3a3a')
 
-ui.markdown('# ***zdkrimson : A Minecraft Launcher***')
+# with ui.image('https://github.com/zdkrimson/zdkrimson.github.io/blob/main/assets/bg/blur/crim1.png?raw=true'):
+
+with ui.card().tight() as card:
+with ui.splitter() as splitter:
+	with splitter.before:
+		ui.image('assets/logo/image/shaded/white.png')
+	with splitter.after:
+		ui.markdown('## ***zdkrimson : A Minecraft Launcher***')
+		ui.label('A Minecraft Laucnher packed with Quality-Of-Life features.')
+
 
 ui.run(dark=True, native=True)
